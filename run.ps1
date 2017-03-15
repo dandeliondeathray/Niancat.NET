959191447e00f41e726051e11a5b9b36504c051d
tlycken <tomas.lycken@gmail.com>
2017-03-15 19:37:56 +0100

    Modify run script for not having a file db

--- run.ps1
+++ run.ps1
@@ -1,16 +1,12 @@
 param(
-    [string] $Wordlist = "ordlista.txt",
-    [string] $EventsFile = "events.txt"
+    [string] $Wordlist = "ordlista.txt"
 )
 
 if (!(Test-Path $Wordlist)) {
     Write-Warning "Wordlist file $Wordlist does not exist; aborting."
     return
 }
-if (!(Test-Path $EventsFile)) {
-    Write-Host "Events file $EventsFile does not exist; it will be created when the first domain event is persisted."
-}
 
-Write-Host "Starting Niancat API with wordlist from $Wordlist, db in $EventsFile"
+Write-Host "Starting Niancat API with wordlist from $Wordlist"
 
-& build/Niancat.Api.exe $Wordlist $EventsFile
+& build/Niancat.Api.exe $Wordlist
